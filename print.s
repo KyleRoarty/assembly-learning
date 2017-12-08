f2caa7f4b16338ae841e06fd5b827c608dc4d46c
Kyle Roarty <kroarty@xes-inc.com>
2017-12-08 13:03:44 -0600

    Testing to print string
    Signed-off-by: Kyle Roarty <kroarty@xes-inc.com>

--- print.s
+++ print.s
@@ -1,17 +1,32 @@
+.text
+    STR0:
+        .ascii "Hello, world!\0"
+
 .global _start
 
 _start:
-    movb $0x0e, %ah
 
-    movb $'H', %al
+    mov $STR0, %ebx
+    mov $1, %edi
+
+label1:
+
+    cmpb $0, (%ebx, %edi)
+    je label2
+
+    movb $0x0e, %ah
+    movb (%ebx, %edi), %al
     int $0x10
-    movb $0x69, %al
-    int $0x10
-    movb $0x21, %al
-    int $0x10
+
+    add $1, %edi
+
+    jmp label1
+
+
+label2:
 
     jmp .
 
-    .fill 510 - (. - _start), 1, 0
+    .fill 510 - (. - .text), 1, 0
     .word 0xaa55
 
