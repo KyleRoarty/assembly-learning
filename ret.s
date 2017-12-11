126edfbc6f4a94f0e4789e938dd53f892c42647a
Kyle Roarty <kyleroarty1716@gmail.com>
2017-12-11 07:58:00 -0600

    ret*: implement hello world in x86 and x64
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- ret.s
+++ ret.s
@@ -1,11 +1,23 @@
 .data
 
+hellostr:
+	.ascii "Hello, World!\n"
+hellolen:
+	.equ len, hellolen - hellostr
+
 .text
 
 .global _start
 
 _start:
 
+	mov		$1, %rax
+	mov		$1, %rdi
+	mov		$hellostr, %rsi
+	mov		$len, %rdx
+	syscall
+
+
 	mov		$60, %rax
-	mov		$0x10, %rdi
+	mov		$0x0, %rdi
 	syscall
--- ret_int.s
+++ ret_int.s
@@ -1,10 +1,20 @@
 .data
 
+hellostr:
+	.ascii "Hello, World!\n"
+helloend:
+	.equ len, helloend - hellostr
+
 .text
 
 .global _start
 
 _start:
+	movl $4, %eax
+	movl $1, %ebx
+	movl $hellostr, %ecx
+	movl $len, %edx
+	int $0x80
 
 	movl $1, %eax
 	movl $100, %ebx
