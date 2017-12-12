1e70344c302c43706808d79316238ee46efeb5bc
Kyle Roarty <kyleroarty1716@gmail.com>
2017-12-11 22:30:23 -0600

    ret: Print out argv's
    go to https://ftp.gnu.org/old-gnu/Manuals/make-3.79.1/html_chapter/make_10.html
    to do cool implicit makefile stuff
    
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- ret.s
+++ ret.s
@@ -1,23 +1,44 @@
 .data
 
-hellostr:
-	.ascii "Hello, World!\n"
-hellolen:
-	.equ len, hellolen - hellostr
+nl:
+	.ascii "\n"
+nlend:
+	.equ len, nlend - nl
 
 .text
 
 .global _start
 
 _start:
+	movq	(%rsp), %r12
+	movq	$0, %r13
+
+.L_loop_start:
+	movq	$1, %rax
+	movq	$1, %rdi
+	movq	8(%rsp, %r13, 8), %rsi
+	movq	%rsi, %r10
+
+.L_str_len:
+	cmpb $0, (%r10)
+	je .L_str_len_end
+	inc %r10
+	jmp .L_str_len
+.L_str_len_end:
+	sub %rsi, %r10
+	movq %r10, %rdx
+	syscall
 
 	movq	$1, %rax
 	movq	$1, %rdi
-	movq	$hellostr, %rsi
+	movq	$nl, %rsi
 	movq	$len, %rdx
 	syscall
 
+	inc %r13
+	cmpq %r12, %r13
+	jne .L_loop_start
 
-	mov		$60, %rax
-	mov		$0x0, %rdi
+	movq	$60, %rax
+	movq	$0x0, %rdi
 	syscall
